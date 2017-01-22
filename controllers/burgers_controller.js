b0fc436589e55e614bb857dc5696c899385991c6
Ritu Dubey <Ritu Dubey>
2017-01-21 23:00:32 -0500

    initial commit

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,45 @@
+var express = require("express");
+
+var router = express.Router();
+// Import the model (cat.js) to use its database functions.
+var cat = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+  cat.all(function(data) {
+    var hbsObject = {
+      cats: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/burgers/create", function(req, res) {
+  cat.create([
+    "name", "sleepy"
+  ], [
+    req.body.name, req.body.sleepy
+  ], function() {
+    res.redirect("/burgers");
+  });
+});
+
+router.put("/burgers/update/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.update({
+    sleepy: req.body.sleepy
+  }, condition, function() {
+    res.redirect("/burgers");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
